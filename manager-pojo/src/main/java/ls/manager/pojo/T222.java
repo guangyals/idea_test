53fbe543090c567bc78cd34a2b1f9c4009f4bf09
liushuai <liushuai@eqxiu.com>
2020-04-13 22:43:03 +0800

    添加2222

---
+++ manager-pojo/src/main/java/ls/manager/pojo/T222.java
@@ -0,0 +1,13 @@
+package ls.manager.pojo;
+
+/**
+ * @Auther: http://www.bjsxt.com
+ * @Date: 2018/11/23 0023
+ * @Description: com.bjsxt.pojo
+ * @version: 1.0
+ */
+public class T222 {
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+    }
+}
